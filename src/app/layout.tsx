e9846f1fbe77c980656a9c564e5e60de62dd2c26
HomesBuiltUniquely <tech@homesunderbudget.com>
2025-07-16 12:16:22 +0530

    final commit5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -93,6 +94,7 @@ export default function RootLayout({
         </noscript>
 
         {children}
+        <Analytics/>
       </body>
     </html>
   );
