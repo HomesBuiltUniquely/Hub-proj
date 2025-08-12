c5fda94ffdc21de1ade9f0c656a625b54551ded9
HomesBuiltUniquely <tech@homesunderbudget.com>
2025-08-12 11:38:43 +0530

    Addingpages1

--- src/app/Components/Life@Hub/Section2.tsx
+++ src/app/Components/Life@Hub/Section2.tsx
@@ -16,7 +16,7 @@ const BedroomSection: React.FC = () => (
       <div className="bg-[#ddcdc1] rounded-3xl px-9 py-10 flex flex-col justify-center md:w-1/2 w-full min-h-[350px]">
         <h2 className="text-3xl md:text-4xl font-extrabold text-amber-950 mb-6">Why to become a HUBSTER?</h2>
         <p className="text-amber-950 text-base md:text-lg leading-relaxed font-normal">
-        Because at HUB, you’re not just joining a team — you&#39;re becoming part of a culture that values passion, creativity, and purpose. Here, you get the freedom to grow, the support to thrive, and the space to bring bold ideas to life. If you're looking for a place where your work makes a difference and every day feels inspired, then HUB is where you belong.
+        Because at HUB, you&#39;re not just joining a team — you&#39;re becoming part of a culture that values passion, creativity, and purpose. Here, you get the freedom to grow, the support to thrive, and the space to bring bold ideas to life. If you're looking for a place where your work makes a difference and every day feels inspired, then HUB is where you belong.
         </p>
       </div>
     </div>
