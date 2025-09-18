b348e2f2be32ca5f5203392397e0fe75a30c3a20
HomesBuiltUniquely <tech@homesunderbudget.com>
2025-09-18 20:14:42 +0530

    added s

--- src/app/Components/Home/Herosection.tsx
+++ src/app/Components/Home/Herosection.tsx
@@ -85,7 +85,7 @@ const Herosection: React.FC = () => {
             className={`w-3 h-3 rounded-full transition-all duration-300 ${
               index === currentSlide 
                 ? 'bg-white scale-110' 
-                : 'bg-white/50 hover:bg-white/70'
+                : 'bg-white/50 hover:bg-white/60'
             }`}
           />
         ))}
