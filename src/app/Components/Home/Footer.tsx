ce0a7ac9595643d8bafbb0c6145240b459cc5ddd
HomesBuiltUniquely <tech@homesunderbudget.com>
2025-09-20 12:08:07 +0530

    adding mobile version3

--- src/app/Components/Home/Footer.tsx
+++ src/app/Components/Home/Footer.tsx
@@ -448,9 +448,9 @@ export default function Footer(){
                                         <span className="text-white text-xs font-bold"> <img src="/linkedin.png " className="w-[25px] h-[25px] "></img></span>
                                     </Link>
                                     <Link href="https://youtube.com" target="_blank" rel="noopener noreferrer" className="w-8 h-8 s rounded-full flex items-center justify-center hover:bg-white/30 transition-colors">
-                                        <svg className="w-4 h-4 text-white" fill="currentColor" viewBox="0 0 24 24">
+                                    <span className="text-white text-xs font-bold w-7 h-7 border-1 border-white rounded-full pt-1.5 pl-1"> <svg className="w-4 h-4 text-white" fill="currentColor" viewBox="0 0 24 24">
                                             <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
-                                        </svg>
+                                        </svg></span>
                                     </Link>
                                     <Link href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="w-8 h-8 rounded-full flex items-center justify-center hover:bg-white/30 transition-colors">
                                     <img src="/instagram.png" className="w-[25px] h-[25px] "></img>
