773a7f8a6f69f91f40739586f067d7d491b894ed
HomesBuiltUniquely <tech@homesunderbudget.com>
2025-10-24 16:59:16 +0530

    underconstruction pages

---
+++ src/app/KidsRoom/page.tsx
@@ -0,0 +1,8 @@
+export default function KidsRoom() {
+  return (
+    <div>
+      <h1>Kids Room</h1>
+      <p>This page is under construction.</p>
+    </div>
+  )
+}
---
+++ src/app/LivingRoom/page.tsx
@@ -0,0 +1,8 @@
+export default function LivingRoom() {
+  return (
+    <div>
+      <h1>Living Room</h1>
+      <p>This page is under construction.</p>
+    </div>
+  )
+}
