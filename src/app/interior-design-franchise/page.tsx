4a54f8876dca2606e8030b94cbf6bbe850c87917
nishanth096 <nishanth@hubinterior.com>
2025-10-24 15:43:40 +0530

    footer changes done

--- src/app/interior-design-franchise/page.tsx
+++ src/app/interior-design-franchise/page.tsx
@@ -3,6 +3,7 @@ import Business from "../Components/Franchise/Business";
 import Home from "../Components/Franchise/Herosection";
 import Footer from "../Components/Home/Footer";
 
+
 export default function Franchise(){
     return(
         <div>
