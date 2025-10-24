4dd7f2059a82835f1e912e568263e91c94c9c376
nishanth096 <nishanth@hubinterior.com>
2025-10-24 18:08:37 +0530

    done with errors

--- src/app/Components/Franchise/Herosection.tsx
+++ src/app/Components/Franchise/Herosection.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import OfferingsDropdown from "../OfferingsDropdown";
