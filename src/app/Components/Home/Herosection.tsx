672fddb49c31e8c7a1404bdac282cda7e04202da
HomesBuiltUniquely <tech@homesunderbudget.com>
2026-01-15 20:05:41 +0530

    added running

--- src/app/Components/Home/Herosection.tsx
+++ src/app/Components/Home/Herosection.tsx
@@ -22,16 +22,33 @@ const Herosection: React.FC = () => {
 
   const heroSlides = [
     {
-      video: "https://hubinterior-quote-2026.s3.ap-south-2.amazonaws.com/Final%20Video.mp4?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIA2UHGCZDIDBGPCJCR%2F20260115%2Fap-south-2%2Fs3%2Faws4_request&X-Amz-Date=20260115T142043Z&X-Amz-Expires=300&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEOf%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCmFwLXNvdXRoLTIiSDBGAiEA3w1RHMTi8xfK4NDGTXHWZxwSnCL9PMRAj91nxcvEygUCIQDsH95WAGPwZoRsKMvGp7X78GErurxmu70rRTAxU%2B1GuyraAgg3EAAaDDczMDYyNjk2Nzc2MCIM4tjd4TdmXqaHPUqOKrcCpCsnklSmOQaJDlTOmWrD8vqfpQ6ocN5bZUzpODlCw%2FC9p36a5bKtAK9oXmSoRMUdM2X%2B57hu86GFnxiTBJsC5RPlXRHLWyenVyKT%2FpmU6wVR0S7RD2fWaJpeXV%2BgTmEXlOkJtY7yb7AWB0Ls30gEDXM2jBcxRrBrj40lNdnINTQ9Mw3inLox2GPWfasftQTwQIKDTB%2BIJpxe6aVJcufEZsOv3o%2FZbreLM%2B0lrDggsKq8nPQHjzd%2B0wi9jRyYvYjlqmbWgWwfvsl3AKpYolNmoX7%2BVc3bqnCVG%2F6kttqWhx%2BW8vmQInucsKc%2FtufxBgZ7SZVPCDmE2L1QzGlhZl1u32ewlAgturUmgzTYTNAkegEs50Rog9QJJCrQuHXnk0ZJb%2FOAALJW1uV7bvefpzkzhbxrcd5bPDgw7IajywY6rALX38E%2F%2FXIwm65qoFWfztL72HGVXi3cpBlUoX%2BgmXGsplXrHXpi50ePKoKa21egOfyOtMr9mzBxC7wGbhbMh7DkGXNohlMHHjhjfs8s1uwFvo3te69pU4AZ%2FEqOvmcYKTbHk0MNG010HQXkDXdwHXZ6xfgClvmSSkIhmg%2FXQTYKXwD80GqxAqgR3loXs39cfOZD%2BV8L53KJG1Eb25gKgjC9%2FILk91n5dLmiRMmTFflFNvhYWM%2FYRoVb9viblv2zBJLAPcvs25dwG%2BVFc2dA2R94Yv5oPpU3D01a7KUNRqEZ02O52UJnXlzc%2Bia%2BKZlLJRj29VghkSn5co1kIRtLstQmIBIOGav8x1DMyYQ5YYp99%2BE2T8%2BuQfvQdBP%2Bz4nFEkr3EPAU697rsEi4jnE%3D&X-Amz-Signature=9a479fde1cc98babad3abb7fef9a3a75938de9f07d30e1afef5e7234de7a5fb7&X-Amz-SignedHeaders=host&response-content-disposition=inline",
+      image: "/img1HH.jpg",
       title: "From floor plan to lifestyle home, uniquely built",
       subtitle: "Innovative interior design",
+      buttonText: "Book Free Consultation"
+    },
+    {
+      image: "/img2HH.jpg",
+      title: "Get your home interior cost estimate today",
+      subtitle: "a Kitchen that inspires",
+      buttonText: "Calculate Now"
+    },
+
+    // hh3.png,img3HH.jpg
+
+    {
+      image: "/img3HH.jpg",
+      title: "From vision to reality, Interiors in 34 days",
+      subtitle: "Living space today",
       buttonText: "Book Free Consultation"
     }
   ];
 
 
   const mobileTexts = [
-    "From floor plan to lifestyle home, uniquely built"
+    "From floor plan to lifestyle home, uniquely built",
+    "Get your home interior cost estimate today",
+    "From vision to reality, Interiors in 34 days"
   ];
 
   // Auto-slide effect
@@ -102,20 +119,15 @@ const Herosection: React.FC = () => {
       {/* 2560 Version */}
 
       <div className="hidden md:block  desktop-2560 w-full max-w-[1440px] h-[900px] mx-auto overflow-hidden relative ">
-        {/* Background Video Carousel */}
+        {/* Background Image Carousel */}
         <div className="absolute inset-0 w-full max-w-8xl h-[full]">
           {heroSlides.map((slide, index) => (
-            <video
+            <div
               key={index}
-              className={`absolute inset-0 w-full h-full object-cover transition-opacity duration-1000 ${index === currentSlide ? 'opacity-100' : 'opacity-0'
+              className={`absolute inset-0 bg-cover transition-opacity duration-1000 ${index === currentSlide ? 'opacity-100' : 'opacity-0'
                 }`}
-              autoPlay
-              muted
-              loop
-              playsInline
-            >
-              <source src={slide.video} type="video/mp4" />
-            </video>
+              style={{ backgroundImage: `url('${slide.image}')` }}
+            />
           ))}
         </div>
 
@@ -194,20 +206,15 @@ const Herosection: React.FC = () => {
 
       {/* 1920 version  */}
       <div className="relative hidden md:block  desktop-1920 w-full max-w-[1920px] h-[900px]  overflow-hidden">
-        {/* Background Video Carousel */}
+        {/* Background Image Carousel */}
         <div className="absolute inset-0 w-full h-[900px]">
           {heroSlides.map((slide, index) => (
-            <video
+            <div
               key={index}
-              className={`absolute inset-0 w-full h-full object-cover transition-opacity duration-1000 ${index === currentSlide ? 'opacity-100' : 'opacity-0'
+              className={`absolute inset-0 bg-cover transition-opacity duration-1000 ${index === currentSlide ? 'opacity-100' : 'opacity-0'
                 }`}
-              autoPlay
-              muted
-              loop
-              playsInline
-            >
-              <source src={slide.video} type="video/mp4" />
-            </video>
+              style={{ backgroundImage: `url('${slide.image}')` }}
+            />
           ))}
         </div>
 
@@ -288,20 +295,15 @@ const Herosection: React.FC = () => {
       {/* 1280 version */}
       <div>
         <div className="desktop-1280 max-w-[1280px] h-[800px] mx-auto overflow-hidden relative">
-          {/* Background Video Carousel */}
+          {/* Background Image Carousel */}
           <div className="absolute inset-0">
             {heroSlides.map((slide, index) => (
-              <video
+              <div
                 key={index}
-                className={`absolute inset-0 w-full h-full object-cover transition-opacity duration-1000 ${index === currentSlide ? 'opacity-100' : 'opacity-0'
+                className={`absolute inset-0 bg-cover bg-center transition-opacity duration-1000  ${index === currentSlide ? 'opacity-100' : 'opacity-0'
                   }`}
-                autoPlay
-                muted
-                loop
-                playsInline
-              >
-                <source src={slide.video} type="video/mp4" />
-              </video>
+                style={{ backgroundImage: `url('${slide.image}')` }}
+              />
             ))}
           </div>
 
@@ -393,15 +395,7 @@ const Herosection: React.FC = () => {
           </div>
 
 
-          <video
-            className="w-full max-w-[360px] h-auto rounded-4xl relative -mt-14 mx-auto object-cover"
-            autoPlay
-            muted
-            loop
-            playsInline
-          >
-            <source src={heroSlides[currentSlide].video} type="video/mp4" />
-          </video>
+          <img className="w-full max-w-[360px] h-auto rounded-4xl relative -mt-14 mx-auto" src={`/hh1${currentSlide + 1}.png`} />
           <div className="-mt-55 w-[300px] text-left text-4xl manrope text-white mx-auto wulkan-display-bold absolute ml-7">{mobileTexts[currentSlide]}</div>
           {/* Horizontal line above button */}
           <div className="w-[295px] h-[1px] bg-gray-300 absolute -mt-23 ml-7"></div>
@@ -423,15 +417,7 @@ const Herosection: React.FC = () => {
                 className="cursor-pointer w-[100px] h-[90px] -ml-1 -mt-5 mb-2" />
             </div>
           </div>
-          <video
-            className="max-w-[425px] h-[500px] rounded-4xl relative -mt-18 mx-auto object-cover"
-            autoPlay
-            muted
-            loop
-            playsInline
-          >
-            <source src={heroSlides[currentSlide].video} type="video/mp4" />
-          </video>
+          <img className="max-w-[425px] h-[500px] rounded-4xl relative -mt-18 mx-auto" src={`/hh1${currentSlide + 1}.png`} />
           <div className="-mt-62 w-[250px] text-left text-3xl manrope text-white mx-auto wulkan-display-bold absolute ml-8">{mobileTexts[currentSlide]}</div>
           {/* Horizontal line above button */}
           <div className="w-[300px]  h-[1px] bg-gray-300 absolute -mt-32 ml-8"></div>
