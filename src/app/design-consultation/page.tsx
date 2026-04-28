128e702a836bafb8280f3b5e3d56d87a2a6d79c1
Vishal Kholakaiya <vishal@hubinterior.com>
2026-04-28 12:33:56 +0530

    removed footer in design-consultation

--- src/app/design-consultation/page.tsx
+++ src/app/design-consultation/page.tsx
@@ -1,11 +1,10 @@
 import DesignConsultationForm from "../Components/DesignConsultation/DesignConsultationForm";
-import Footer from "../Components/Home/Footer";
+
 
 export default function DesignConsultationPage() {
   return (
     <div>
       <DesignConsultationForm />
-      <Footer />
     </div>
   );
 }
